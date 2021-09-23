8e181e69bf420fe94248d8ee96b8dcc3546b7c16
Shelby Gleason <shelbygleason@Shelbys-MacBook-Air.local>
2021-09-22 19:09:00 -0600

    updates

---
+++ images/logo_files/environment-6e9a6e9df896ba9d78ddceaa68a9e5bd.js
@@ -0,0 +1,3 @@
+define("trademark/config/environment",(function(){return{default:{modulePrefix:"trademark",podModulePrefix:"trademark/pods",rootURL:"/",locationType:"auto","ember-component-css":{terseClassNames:!1}}}}))
+
+//# sourceMappingURL=environment.map
